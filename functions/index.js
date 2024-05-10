2bd587351220a7e545aed0b219f07b0d1b225eba
lrnzznn1 <lorenzo.zanini@studenti.unitn.it>
2024-05-10 23:18:17 +0200

    Project

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,50 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// Configura il trasportatore per l'invio delle email (puoi utilizzare qualsiasi servizio di email supportato da nodemailer)
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // true per connessione SSL/TLS
+  auth: {
+    user: "eventlinkauth@gmail.com", // Inserire qui la propria email
+        pass: "pulmbdohgumgpxwo", // Inserire qui la propria password
+  }
+});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Funzione per gestire la richiesta POST
+exports.handlePostRequest = functions.https.onRequest((request, response) => {
+  // Verifica che la richiesta sia di tipo POST
+  if (request.method !== 'POST') {
+    return response.status(400).json({ error: 'Solo richieste POST sono supportate' });
+  }
+
+  // Estrai i dati dalla richiesta POST
+  const data = request.body;
+
+  // Verifica se sono presenti i campi richiesti
+  if (!data || !data.email || !data.text) {
+    return response.status(400).json({ error: 'I campi email e text sono obbligatori' });
+  }
+
+  // Configura l'email da inviare
+  const mailOptions = {
+    from: 'EventLinkAuth@gmail.com',
+    to: data.email, // Indirizzo email destinatario
+    subject: 'Prova',
+    text: data.text // Testo dell'email
+  };
+
+  // Invia l'email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Errore durante l\'invio dell\'email:', error);
+      return response.status(500).json({ error: 'Si è verificato un errore durante l\'invio dell\'email' });
+    } else {
+      console.log('Email inviata con successo:', info.response);
+      return response.status(200).json({ message: 'Email inviata con successo' });
+    }
+  });
+});
